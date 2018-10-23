0f9b96067879cf024c732508df95c10549a3a842
Peter <ochenx@gmail.com>
2018-10-23 13:38:59 +0200

    changed var to let

--- script.js
+++ script.js
@@ -1,74 +1,73 @@
- 
-        // czas poczatkowy
-        var start = new Date().getTime();
+// czas poczatkowy
+let start = new Date().getTime();
 
-        // tworzy shape
-            function makeShapeAppear() {
+// tworzy shape
+function makeShapeAppear() {
 
-                var top = Math.random() * 400;
-                var left = Math.random() * 400;
-                var width = (Math.random() * 200)+20;
-                
-                if (Math.random() > 0.5) {
-                    document.getElementById("shape").style.borderRadius="50%";
-                }
-                else
-                {
-                    document.getElementById("shape").style.borderRadius="0";
-                }
-                //var height = Math.random() * (30,300);
-                //document.getElementById("shape").style.height = height + "px";
+    let top = Math.random() * 400;
+    let left = Math.random() * 400;
+    let width = (Math.random() * 200) + 20;
 
-                document.getElementById("shape").style.backgroundColor = getRandomColor();
-                
-                document.getElementById("shape").style.width = width + "px";
+    if (Math.random() > 0.5) {
+        document.getElementById("shape").style.borderRadius = "50%";
+    }
+    else {
+        document.getElementById("shape").style.borderRadius = "0";
+    }
+    //var height = Math.random() * (30,300);
+    //document.getElementById("shape").style.height = height + "px";
 
-                document.getElementById("shape").style.height = width + "px";
+    document.getElementById("shape").style.backgroundColor = getRandomColor();
 
-                document.getElementById("shape").style.top = top + "px";
+    document.getElementById("shape").style.width = width + "px";
 
-                document.getElementById("shape").style.left = left + "px";
-                
-                document.getElementById("shape").style.display = "block";
+    document.getElementById("shape").style.height = width + "px";
 
-                start = new Date().getTime(); 
+    document.getElementById("shape").style.top = top + "px";
 
-            }
-        // losowy kolor
-            function getRandomColor() {
-                var allowable = "0123456789abcdef";
-                var color = "#"
-                for (var i=0; i<6; i++)
-                {
-                    color += allowable[Math.floor(Math.random() * 16)];
-                }
-                return color;
-            }
+    document.getElementById("shape").style.left = left + "px";
 
-        // opoznienie w milisekundach
-            function appearAfterDelay() {
+    document.getElementById("shape").style.display = "block";
 
-            setTimeout(makeShapeAppear, Math.random() * 2000);
+    start = new Date().getTime();
 
-            }
-        // wywolanie metody(funkcji)
-            appearAfterDelay();
+}
 
-        // obsluga znikania ksztaltow
-            document.getElementById("shape").onclick = function() {
+// losowy kolor
+function getRandomColor() {
+    let allowable = "0123456789abcdef";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += allowable[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-                document.getElementById("shape").style.display = "none";
+// opoznienie w milisekundach
+function appearAfterDelay() {
 
-                var end = new Date().getTime();
+    setTimeout(makeShapeAppear, Math.random() * 2000);
 
-                var timeTaken = (end - start) / 1000;
+}
 
-                document.getElementById("time").innerHTML = (timeTaken) + "s";
+// wywolanie metody(funkcji)
+appearAfterDelay();
 
-                start = new Date().getTime();
+// obsluga znikania ksztaltow
+document.getElementById("shape").onclick = function () {
 
-                appearAfterDelay();
-    
-            }
+    document.getElementById("shape").style.display = "none";
+
+    let end = new Date().getTime();
+
+    let timeTaken = (end - start) / 1000;
+
+    document.getElementById("time").innerHTML = (timeTaken) + "s";
+
+    start = new Date().getTime();
+
+    appearAfterDelay();
+
+};
         
         
